d65fa3c5ab562deb79c44644e9ebd9a8eb440d50
RWParsons <rex.parsons94@gmail.com>
2024-08-19 15:09:28 +1000

    do some light rounding when testing snaps
    there seems to be some tiny differences in the testing snaps occurring on GHA tests causing a failure - this change ensures the dimensions and practical differences are still tested but should pass tests where there are irrelevant differences occuring.

--- tests/testthat/helper-tbl-snaps.R
+++ tests/testthat/helper-tbl-snaps.R
@@ -2,6 +2,12 @@ expect_tbl_snap <- function(x, sample_n = 100, seed = 42) {
   # this function is only ever used within the testing suite
   withr::local_seed(seed)
 
+  x <- x |>
+    dplyr::mutate(dplyr::across(
+      dplyr::where(is.numeric),
+      ~ round(.x, digits = 4)
+    ))
+
   df_sample <- x |>
     dplyr::slice_sample(n = sample_n) |>
     as.data.frame()
