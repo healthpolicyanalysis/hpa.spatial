34ab7863e626b18d9e38875609d0ac6173741368
RWParsons <rex.parsons94@gmail.com>
2024-08-20 17:02:21 +1000

    update test-get_correspondence_tbl

--- tests/testthat/test-get_correspondence_tbl.R
+++ tests/testthat/test-get_correspondence_tbl.R
@@ -28,29 +28,19 @@ test_that("can get CG via make_correspondence_tbl() and retrieve faster a second
   expect_lt(res$second_call, res$first_call) # second retrieval should be faster than first
 })
 
+test_that("can get CG using input polygons rather than areas/years and using a custom CG", {
+  test_mb <- readRDS(test_path("fixtures", "brisbane_west_mb.rds"))
 
-test_that("can get a custom CG via make_correspondence_tbl()", {
-  tbl <- suppressWarnings(get_correspondence_tbl(from_area = "sa2", from_year = 2011, to_area = "LHN"))
-
-  expect_tbl_snap(tbl)
-})
-
-test_that("can get CG using input polygons rather than areas/years", {
-  from_sa2 <- suppressWarnings(get_polygon(name = "sa32016", crs = 7844))
+  from_sa2 <- suppressWarnings(get_polygon(name = "sa32016", crs = 7844)) |>
+    dplyr::filter(sa4_name_2016 == "Brisbane - West")
   to_lhn <- get_polygon(name = "LHN")
 
   tbl <- get_correspondence_tbl(
     from_geo = from_sa2,
     to_geo = to_lhn,
-    export_fname = "sa2-to-lhn"
+    export_fname = "sa2-to-lhn",
+    mb_geo = test_mb$pop
   )
-
-  expect_tbl_snap(tbl)
-})
-
-test_that("correspondence table is complete - all non-NA SA2 mappings are other territories", {
-  tbl <- suppressWarnings(get_correspondence_tbl(from_area = "sa2", from_year = 2021, to_area = "LHN")) |>
-    dplyr::filter(is.na(LHN_Name))
 
   expect_tbl_snap(tbl)
 })
