0ed61094f1d7e198117c498be6b63668ab7c8e55
RWParsons <rex.parsons94@gmail.com>
2024-08-23 13:24:57 +1000

    Update test-read_hpa_spatial_data.R

--- tests/testthat/test-read_hpa_spatial_data.R
+++ tests/testthat/test-read_hpa_spatial_data.R
@@ -1,6 +1,4 @@
 test_that("downloading data from hpa.spatial.data works", {
-  expect_s3_class(read_hpa_spatial_data("phn"), "sf")
-  expect_s3_class(read_hpa_spatial_data("lhn"), "sf")
-  expect_s3_class(read_hpa_spatial_data("mb21_poly"), "sf")
-  expect_s3_class(read_hpa_spatial_data("mb21_pop"), "sf")
+  expect_s3_class(read_hpa_spatial_data("phn"), "sf") # test normal shapes
+  expect_s3_class(read_hpa_spatial_data("mb21_poly"), "sf") # test partitioned shapes
 })
